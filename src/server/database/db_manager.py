6faf23113b91133e532fe9b948406e91fd76ca85
Inf3rn01 <spaceyt235@gmail.com>
2023-10-05 14:22:46 +0300

    1

---
+++ src/server/database/db_manager.py
@@ -0,0 +1,16 @@
+import sqlite3
+import os
+import sys
+
+sys.path.append("C:\\firm-resurs-miner\src")
+
+import settings
+
+class DBManager:
+    def __init__(self,default_path: str):
+        self.default_path = default_path
+    
+    def connect_to_db():
+        pass
+
+
---
+++ src/settings.py
@@ -0,0 +1,2 @@
+#Database settings
+PATH = "C:/firm-resurs-miner/src/server/database/mining_firm.db"
